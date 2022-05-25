55d11d551384e39894707d334a48c63bb012d599
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-05-25 18:36:21 -0400

    May 25, 2022 6:36 PM

--- APGD.sol
+++ APGD.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.7;
 
 contract PhysicalActivityMonitorGame {
     mapping(address => uint256) public balance;
+
+    struct DailyActivity{
+        uint256 steps;
+        uint256 calories;
+        uint256 distance;
+        uint256 floors;
+        uint256 activeMinutes;
+        uint256 sedentaryMinutes;
+    }
 }
