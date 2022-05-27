fe2645039663b5b2cc35304f4a20aa4fb39f0a74
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-05-27 11:46:05 -0400

    May 27, 2022 11:46 AM

--- APGD.sol
+++ APGD.sol
@@ -37,7 +37,7 @@ contract AGPD {
     function EnterActivity(uint256 ActivityType, uint256 Minutes) public returns(uint256 TokensMinted, bool success){
         require(ActivityType <= 7 && ActivityType >= 1);
         require(Minutes >= 1);
-        
+
     }
 
 
