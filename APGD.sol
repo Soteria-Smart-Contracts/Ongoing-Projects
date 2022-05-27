117175e73ebc0c5de5070d44cb75f218f18c92a2
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-05-27 12:26:43 -0400

    May 27, 2022 12:26 PM

--- APGD.sol
+++ APGD.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.7;
 import "StandardERC20.sol";
 
 contract AGPD {
-    address HealthyEthersToken = address(0);
+    address HealthyEthers = address(0);
 
     constructor(){
         //Deployment Constructors
@@ -51,6 +51,7 @@ contract AGPD {
         require(Minutes >= 1);
         
         uint256 TokensEarned = TypeMultiplier[ActivityType] * Minutes;
+        
 
 
 
