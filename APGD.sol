191fdf3523a26a0895cfe9deb0c06671c9d8b869
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-05-27 11:44:29 -0400

    May 27, 2022 11:44 AM

--- APGD.sol
+++ APGD.sol
@@ -7,9 +7,36 @@ import "StandardERC20.sol";
 
 contract AGPD {
     address Zero = address(0);
+    
+    
+
+    //Struct Declarations
+    struct ActivityHistory{
+        //In Minutes
+        uint256 Walking;
+        uint256 Running;
+        uint256 Cycling;
+        uint256 Swimming;
+        uint256 GymWorkout;
+        uint256 TeamSports;
+        uint256 Other;
+    }
+
+    //Activity Types are as such:
+    // 1: Walking
+    // 2: Running
+    // 3: Cycling
+    // 4: Swimming
+    // 5: Gym Workout
+    // 6: Team Sports
+    // 7: Other
 
     //Mapping Declarations
-    
+    mapping(address => ActivityHistory) Tracking;
+
+    function EnterActivity(uint256 ActivityType, uint256 Minutes) public returns(uint256 TokensMinted, bool success){
+        require(ActivityType <= 7 && ActivityType >= 1);
+    }
 
 
     
