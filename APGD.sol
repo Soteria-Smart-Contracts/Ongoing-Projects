1d92560cf26e0adfa874113a57345feb0e986b85
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-05-27 21:52:15 -0400

    push

--- APGD.sol
+++ APGD.sol
@@ -17,11 +17,18 @@ contract AGPD {
         TypeMultiplier[5] = 3;
         TypeMultiplier[6] = 5;
         TypeMultiplier[7] = 4;
+        TypeString[1] = "Walking";
+        TypeString[2] = "Running";
+        TypeString[3] = "Cycling";
+        TypeString[4] = "Swimming";
+        TypeString[5] = "GymWorkout";
+        TypeString[6] = "TeamSports";
+        TypeString[7] = "Other";
     }
 
     
 
-    //Struct Declarations
+//Struct Declarations
     struct ActivityHistory{
         //In Minutes
         uint256 Walking;
@@ -45,17 +52,67 @@ contract AGPD {
     //Mapping Declarations
     mapping (address => ActivityHistory) Tracking;
     mapping (uint256 => uint256) TypeMultiplier;
+    mapping (uint256 => string) TypeString;
 
     function EnterActivity(uint256 ActivityType, uint256 Minutes) public returns(uint256 TokensMinted, bool success){
         require(ActivityType <= 7 && ActivityType >= 1, "Activity Type is not valid");
         require(Minutes >= 1);
+        string memory TypeS = TypeString[ActivityType];
         
         uint256 TokensEarned = TypeMultiplier[ActivityType] * Minutes;
-        
+        UpdateTrackingMinutes(ActivityType, Minutes);
 
 
 
 
+    }
+
+
+
+
+
+
+
+
+//()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()
+//Internal Functions
+
+
+//()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()
+//Internal Functions
+
+    function UpdateTrackingMinutes(uint256 ActivityType, uint256 Minutes) internal {
+        require(ActivityType <= 7 && ActivityType >= 1, "Activity Type is not valid");
+        require(Minutes >= 1);
+
+        if (ActivityType == 1);
+        {
+            Tracking[msg.sender].Walking = Tracking[msg.sender].Walking + Minutes;
+        }
+        else if (ActivityType == 2);
+        {
+            Tracking[msg.sender].Running = Tracking[msg.sender].Running + Minutes;
+        }
+        else if (ActivityType == 3);
+        {
+            Tracking[msg.sender].Cycling = Tracking[msg.sender].Cycling + Minutes;
+        }
+        else if (ActivityType == 4);
+        {
+            Tracking[msg.sender].Swimming = Tracking[msg.sender].Swimming + Minutes;
+        }
+        else if (ActivityType == 5);
+        {
+            Tracking[msg.sender].GymWorkout = Tracking[msg.sender].GymWorkout + Minutes;
+        }
+        else if (ActivityType == 6);
+        {
+            Tracking[msg.sender].TeamSports = Tracking[msg.sender].TeamSports + Minutes;
+        }
+        else if (ActivityType == 7);
+        {
+            Tracking[msg.sender].Other = Tracking[msg.sender].Other + Minutes;
+        }
     }
 
 
