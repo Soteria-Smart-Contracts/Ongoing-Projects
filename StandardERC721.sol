67518e958a881328e7296c18dc556b12ad55380f
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-27 13:04:38 -0400

    July 27, 2022 1:04 PM

--- StandardERC721.sol
+++ StandardERC721.sol
@@ -1313,6 +1313,13 @@ contract ClassicRewards is ERC721Enumerable, Ownable {
     string memory _initBaseURI
   ) ERC721(_name, _symbol) {
     setBaseURI(_initBaseURI);
+    uint256 index = 0;
+    uint256 idincrement = 1;
+    while(index <= 9999){
+      UnMinted[index] = idincrement;
+      index++;
+      idincrement++;
+    }
     
   }
 
